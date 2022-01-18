3c9ee2619fa17d04d0612aedcac8c5abe8216110
Lucas Dreher <lucasdreher@gmail.com>
2022-01-18 18:09:32 +0100

    add ref={inputRef}

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -18,7 +18,14 @@ const Input = React.forwardRef((props, ref) => {
 	return (
 		<div className={`${classes.control} ${props.isValid === false ? classes.invalid : ''}`}>
 			<label htmlFor={props.id}>{props.label}</label>
-			<input type={props.type} id={props.id} value={props.value} onChange={props.onChange} onBlur={props.onBlur} />
+			<input
+				ref={inputRef}
+				type={props.type}
+				id={props.id}
+				value={props.value}
+				onChange={props.onChange}
+				onBlur={props.onBlur}
+			/>
 		</div>
 	);
 });
